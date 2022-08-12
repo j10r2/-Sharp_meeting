5e3910c742819979c1247d3000ef42413f6201ea
Anton <j10r2@mail.ru>
2022-08-12 21:57:09 +0300

    ex2

---
+++ семинар 3/ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите целое число:");
+int a = int.Parse(Console.ReadLine());
+int index = 1;
+while (index <= Math.Abs(a))
+{
+    Console.Write(index * index);
+    if (index < Math.Abs(a))
+    {
+        Console.Write(", ");
+    }
+    index++;
+}
