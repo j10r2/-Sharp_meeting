1e614c24b02ce309ced9ce403c4ef77755eca327
Anton <j10r2@mail.ru>
2022-08-13 10:35:44 +0300

    homework exercise 3

---
+++ семинар 3/homework/hw3/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("попробуй ввести любое целое число с трёх попыток (лучше небольшое):");
+int count = 0;
+while (count < 3)
+    try
+    {
+        int a = int.Parse(Console.ReadLine());
+        int index = 1;
+        while ((index < a) || (index > a))
+        {
+            Console.Write(Math.Pow(index, 3));
+            if ((index < a) || (index > a))
+            {
+                Console.Write(", ");
+            }
+            if (a < 0)
+            {
+                index--;
+            }
+            else
+            {
+                index++;
+            }
+            if (index == a)
+            {
+                Console.Write(Math.Pow(index, 3));
+            }
+        }
+        break;
+    }
+    catch (System.Exception)
+    {
+        if (count < 2)
+        {
+            Console.WriteLine("Не получилось, давай ещё раз");
+        }
+        else
+        {
+            Console.WriteLine("А я так в тебя верил...");
+        }
+        count ++;
+    }
+
