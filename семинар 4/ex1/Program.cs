0fb8ebf0e2f4b99354334fe5e2442d54fdc6f384
Anton <j10r2@mail.ru>
2022-08-16 21:55:19 +0300

    classwork

---
+++ семинар 4/ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Console.WriteLine("введите целое число больше 0");
+// int a = int.Parse(Console.ReadLine());
+// int length = 0;
+// if (a / 10 == 0)
+// {
+//     Console.WriteLine("в числе одна цифра");
+// }
+// else
+// {
+//     while (a > 0)
+// {
+//     a = a / 10;
+//     length ++;
+//     Console.WriteLine(length);
+// }
+// Console.WriteLine($"число состоит из { length } знаков");
+// }
+
+Console.WriteLine("введите целое число больше 0");
+int n = int.Parse(Console.ReadLine());
+int prod = 1;
+for (int i = 2; i <= n; i++)
+{
+    prod = prod * i;
+}
+Console.WriteLine(prod);
---
+++ семинар 4/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿void PrintArr (int [] array )
+{
+    int length = array.Length;
+    for (int count = 0; count < length; count++)
+    {
+        Console.Write(array[count]);
+    if (count < 7)
+    {
+        Console.Write(", ");
+    }
+    }
+}
+
+int [] arr = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    arr [i] = new Random().Next(0, 2);
+}
+
+PrintArr(arr);
