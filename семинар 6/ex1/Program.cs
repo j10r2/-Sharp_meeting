3edeb4560039dd4a14b32e01a013b42507b19ca6
Anton <j10r2@mail.ru>
2022-08-24 20:46:55 +0300

    семинар 6

---
+++ семинар 6/ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите длину 1-й стороны треугольника");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите длину 2-й стороны треугольника");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите длину 3-й стороны треугольника");
+int c = int.Parse(Console.ReadLine());
+
+if (a < b + c && b < a + c && c < a + b)
+{
+    Console.WriteLine("треугольник с такими сторонами существовать может");
+}
+else
+{
+    Console.WriteLine("треугольник с такими сторонами не существует");
+}
---
+++ семинар 6/ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число N");
+int N = int.Parse(Console.ReadLine());
+int firstDigit = 0;
+int secondDigit = 1;
+Console.Write("0 1 ");
+if (N > 2)
+{
+    
+int c;
+for (int i = 2; i < N; i ++)
+{
+    c = firstDigit + secondDigit;
+    Console.Write(c + " ");
+    firstDigit = secondDigit;
+    secondDigit = c;
+}
+}
