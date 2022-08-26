2adf549a4a77cf07e08df25328078e8085686347
Anton <j10r2@mail.ru>
2022-08-26 16:55:46 +0300

    homework 6 (done)

---
+++ семинар 6/homework/hw1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("сколько чисел будем вводить?");
+int count = 0;
+int M;
+while (true)
+{
+    try // проверка ввода
+    {
+        M = int.Parse(Console.ReadLine());
+        if (M > 7)
+        {
+            M = 7;
+            Console.WriteLine("Плохо быть жадным. вводи 7 чисел");
+        }
+        break;
+    }
+    catch (System.Exception) //обработка исключений
+    {
+        switch (count)
+        {
+            case 0:
+                Console.WriteLine("ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+                break;
+            default:
+                Console.WriteLine("(x_x)");
+                break;
+        }
+        count++;
+    }
+}
+
+// int RecursiveCount(int s, int col = 0) 
+// {
+//     if (int.Parse(Console.ReadLine()) > 0) col++;
+//     Console.WriteLine(col);
+//     s = s - 1;
+//     if (s == 0) return col;
+//         RecursiveCount(s);
+//         return 0;
+// }
+/*неудачная попытка решить через рекурсию. 
+Зато выяснил, что рекурсия не передаёт значение переменной между уровнями*/
+
+if (M <= 0)
+{
+    Console.WriteLine("Не хочешь - как хочешь");
+}
+else
+{
+    Console.WriteLine("вводи по порядку:");
+    int col = 0;
+    for (int i = 0; i < M; i++) { if (Convert.ToDouble(Console.ReadLine()) > 0) col++; }
+    //можно было бы проверить ввод, но допустим, что пользователь попался адекватный
+    Console.WriteLine($"{col} из введённых чисел больше 0");
+}
+
+//а без обработки исключений было бы 6 строчек кода...
---
+++ семинар 6/homework/hw2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 43:
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:
+y = k1 * x + b1, 
+y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.WriteLine("ищем точку пересечения уравнений:");
+Console.WriteLine("y = k1 * x + b1");
+Console.WriteLine("y = k2 * x + b2");
+Console.WriteLine();
+double k1; double b1; double k2; double b2; /*жаль, что в цикле нельзя объявить :(*/
+while (true) //проверим ввод
+{
+    try
+    {
+        Console.Write("Задайте коэффициент k1: ");
+        k1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Задайте коэффициент b1: ");
+        b1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Задайте коэффициент k2: ");
+        k2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Задайте коэффициент b2: ");
+        b2 = Convert.ToDouble(Console.ReadLine());
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("заново");
+    }
+}
+
+try
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+    Console.WriteLine($"Прямые пересекаются в точке с координатами x = {x}, y = {y}");
+}
+catch (System.Exception)
+{
+    Console.WriteLine("при введённых коэффициентах система не имеет решений");
+}
