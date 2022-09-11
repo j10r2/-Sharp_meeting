f1d371e437c7687a3860ee37fc94ec574f546ed1
Anton <j10r2@mail.ru>
2022-09-11 18:56:13 +0300

    C# last commit :(

---
+++ семинар 9/homework/additional_hw1/Program.cs
@@ -0,0 +1,97 @@
+﻿/* Задача 57 Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+1, 2, 3
+4, 6, 1
+2, 1, 6
+
+
+1 встречается 3 раза
+2 встречается 2 раз
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза */
+
+void Fill2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-5, 15);
+        }
+    }
+}
+void PrintDictionaryArray(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if ((arr[0, j] != 0) && (arr[1, j] != 0)) Console.WriteLine($"{arr[0, j]} встречается в массиве {arr[1, j]} раз(-а)");
+    }
+}
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j]);
+            if (j < arr.GetLength(1) - 1) Console.Write("|");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] FrequencyDictionary(int[,] arr)
+{
+    int[,] frequencyDictionary = new int[2, (arr.GetLength(0) * arr.GetLength(1))];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int finder = 0; finder < (arr.GetLength(0) * arr.GetLength(1)); finder++)
+            {
+                if (frequencyDictionary[0, finder] == arr[i, j])
+                {
+                    frequencyDictionary[1, finder]++;
+                    break;
+                }
+                else if ((frequencyDictionary[0, finder] == 0) && (frequencyDictionary[1, finder] == 0))
+                {
+                    frequencyDictionary[0, finder] = arr[i, j];
+                    frequencyDictionary[1, finder]++;
+                    break;
+                }
+            }
+        }
+    }
+    return frequencyDictionary;
+}
+void SortRowsToMax(int[,] arr)
+{
+    int k = 1;
+    while (k < arr.GetLength(1))
+    {
+        for (int j = 0; j < arr.GetLength(1) - k; j++)
+        {
+            if (arr[0, j] > arr[0, j + 1])
+            {
+                int temp0 = arr[0, j + 1];
+                arr[0, j + 1] = arr[0, j];
+                arr[0, j] = temp0;
+                int temp1 = arr[1, j + 1];
+                arr[1, j + 1] = arr[1, j];
+                arr[1, j] = temp1;
+            }
+        }
+        k++;
+    }
+}
+
+int[,] array = new int[3, 4];
+Fill2DArray(array);
+Print2DArray(array);
+Console.WriteLine();
+
+int[,] dictionary = FrequencyDictionary(array);
+
+SortRowsToMax(dictionary);
+PrintDictionaryArray(dictionary);
---
+++ семинар 9/homework/additional_hw2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*                  Задача 61 
+Вывести первые N строк треугольника Паскаля.
+Сделать вывод в виде равнобедренного треугольника
+
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1               */
+
+Console.WriteLine("введите целое число N (больше 0)");// после 5 строки всё начнёт съезжать. И 
+int N;
+while (true)
+    try
+    {
+        N = int.Parse(Console.ReadLine());
+        if (N > 1) break;
+        Console.WriteLine("попробуй ещё раз");
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("введи целое число больше 1");
+    }
+
+int[,] PascalTriangle(int rowsNumber)
+{
+    int[,] array = new int[rowsNumber, rowsNumber];
+    for (int i = 0; i < rowsNumber; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i) array[i, j] = 1;
+            else array[i, j] = array[i - 1, j - 1] + array[i - 1, j];
+        }
+    }
+    return array;
+}
+void PrintTriangle(int[,] arr)
+{
+    Console.Clear();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        string length = string.Empty;
+        for (int space = 0; space < arr.GetLength(0); space++)
+        {
+            if (arr[i, space] == 0) break;
+            length += Convert.ToString(arr[i, space]);
+            if (space < i) length += " ";
+            // Console.Write(" ");
+        }
+        Console.SetCursorPosition((Console.WindowWidth - length.Length)/2, i + 1);
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == 0) break;
+            Console.Write(arr[i, j]);
+            if (j < arr.GetLength(1) - 1) Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintTriangle(PascalTriangle(N));
+Console.WriteLine();
